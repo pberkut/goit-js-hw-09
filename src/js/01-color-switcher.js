1ad1a7da2a6880fe27a9a34e4d0f93c81b6cbed3
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-27 19:32:20 +0200

    Finish Task # 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+const INTERVAL_TIMER = 1000;
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_TIMER);
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
